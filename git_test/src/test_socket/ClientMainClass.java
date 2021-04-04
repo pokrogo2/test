ba6e5cc5d32b001ccbc5a9f28467401593f2ddd0
pokrogo2 <pokrogo@gmail.com>
2021-04-04 19:12:06 +0900

    test

---
+++ git_test/src/test_socket/ClientMainClass.java
@@ -0,0 +1,10 @@
+package test_socket;
+
+public class ClientMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ git_test/src/test_socket/ServerMainClass.java
@@ -0,0 +1,10 @@
+package test_socket;
+
+public class ServerMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
