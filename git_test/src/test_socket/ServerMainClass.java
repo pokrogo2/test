0bd2975e15b7e86a7e93fadbe9d939d1c40e8604
pokrogo2 <pokrogo@gmail.com>
2021-04-04 19:21:45 +0900

    test3

--- git_test/src/test_socket/ServerMainClass.java
+++ git_test/src/test_socket/ServerMainClass.java
@@ -5,7 +5,7 @@ public class ServerMainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("¾È³ç");
+		System.out.println("ì•ˆë…•");
 	}
 
 }
